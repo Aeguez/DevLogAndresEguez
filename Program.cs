ff0082729b98d073e811201b3f80eb22f836c9c7
Aeguez <andreseguezdavid13@gmail.com>
2022-12-01 15:36:45 -0700

    changes

--- Program.cs
+++ Program.cs
@@ -32,16 +32,6 @@
 using System.Diagnostics;
 using System;
 
-enum chessPiece
-{
-    Knight,
-    King,
-    Rook,
-    Bishop1,
-    Bishop2,
-    Queen
-
-}
 
 namespace Chess
 {
@@ -87,6 +77,7 @@ namespace Chess
         {
             Cell currentCell = new Cell(currentRow, currentCol);
             string chessPiece = "";
+            string colorChessPiece = "";
 
             Console.WriteLine("Enter the piece:");
             try
@@ -97,6 +88,16 @@ namespace Chess
             {
                 Console.WriteLine("Not valid.");
             }
+            Console.WriteLine("Enter the color of the piece:");
+            try
+            {
+                colorChessPiece = Console.ReadLine();
+            }
+            catch
+            {
+                Console.WriteLine("Not valid.");
+            }
+
 
             for (int i = 0; i < Size; i++)
             {
@@ -107,36 +108,51 @@ namespace Chess
                 }
 
             }
+            if (colorChessPiece == "White")
+            {
             switch (chessPiece)
             {
 
                 case "Knight":
-                    theGrid[currentCell.RowNumber + 2, currentCell.ColumnNumber + 1].LegalNextMove = true;
-                    theGrid[currentCell.RowNumber + 2, currentCell.ColumnNumber - 1].LegalNextMove = true;
+                    Console.WriteLine("A knight moves to one of the nearest squares not on the same rank, file, or diagonal.");
+                    if((currentCell.RowNumber > 2 && currentCell.RowNumber < 7) && (currentCell.ColumnNumber > 2 && currentCell.ColumnNumber < 7))
+                    {
                     theGrid[currentCell.RowNumber - 2, currentCell.ColumnNumber + 1].LegalNextMove = true;
                     theGrid[currentCell.RowNumber - 2, currentCell.ColumnNumber - 1].LegalNextMove = true;
+                    theGrid[currentCell.RowNumber + 2, currentCell.ColumnNumber + 1].LegalNextMove = true;
+                    theGrid[currentCell.RowNumber + 2, currentCell.ColumnNumber - 1].LegalNextMove = true;
                     theGrid[currentCell.RowNumber + 1, currentCell.ColumnNumber + 2].LegalNextMove = true;
                     theGrid[currentCell.RowNumber + 1, currentCell.ColumnNumber - 2].LegalNextMove = true;
                     theGrid[currentCell.RowNumber - 1, currentCell.ColumnNumber + 2].LegalNextMove = true;
                     theGrid[currentCell.RowNumber - 1, currentCell.ColumnNumber - 2].LegalNextMove = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Not valid");
+                    }
                     break;
 
                 case "King":
+                    Console.WriteLine("The King is a slow piece that can move only one step in every direction forward, backward, to the sides or diagonall");
+                    if((currentCell.RowNumber >= 1 && currentCell.RowNumber <= 7) && (currentCell.ColumnNumber > 0 && currentCell.ColumnNumber < 7))
+                    {
+                    theGrid[currentCell.RowNumber - 1, currentCell.ColumnNumber + 0].LegalNextMove = true;
+                    theGrid[currentCell.RowNumber - 1, currentCell.ColumnNumber + 1].LegalNextMove = true;
+                    theGrid[currentCell.RowNumber - 1, currentCell.ColumnNumber - 1].LegalNextMove = true;
                     theGrid[currentCell.RowNumber + 1, currentCell.ColumnNumber + 0].LegalNextMove = true;
                     theGrid[currentCell.RowNumber + 1, currentCell.ColumnNumber + 1].LegalNextMove = true;
                     theGrid[currentCell.RowNumber + 1, currentCell.ColumnNumber - 1].LegalNextMove = true;
                     theGrid[currentCell.RowNumber + 0, currentCell.ColumnNumber + 1].LegalNextMove = true;
                     theGrid[currentCell.RowNumber + 0, currentCell.ColumnNumber - 1].LegalNextMove = true;
-                    theGrid[currentCell.RowNumber - 1, currentCell.ColumnNumber + 0].LegalNextMove = true;
-                    theGrid[currentCell.RowNumber - 1, currentCell.ColumnNumber + 1].LegalNextMove = true;
-                    theGrid[currentCell.RowNumber - 1, currentCell.ColumnNumber - 1].LegalNextMove = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Not valid");
+                    }
                     break;
 
                 case "Rook":
-                    theGrid[currentCell.RowNumber + 7, currentCell.ColumnNumber + 0].LegalNextMove = true;
-                    theGrid[currentCell.RowNumber - 7, currentCell.ColumnNumber + 0].LegalNextMove = true;
-                    theGrid[currentCell.RowNumber + 0, currentCell.ColumnNumber + 7].LegalNextMove = true;
-                    theGrid[currentCell.RowNumber + 0, currentCell.ColumnNumber - 7].LegalNextMove = true;
+                    Console.WriteLine("A rook moves any number of vacant squares horizontally or verticallyl.");
                     break;
 
                 case "Bishop":
@@ -154,6 +170,71 @@ namespace Chess
                 default:
                     Console.WriteLine("Invalid choice");
                     break;
+            }
+            }
+            else if(colorChessPiece == "Black")
+            {
+                switch (chessPiece)
+            {
+
+                case "Knight":
+                    Console.WriteLine("A knight moves to one of the nearest squares not on the same rank, file, or diagonal.");
+                     if((currentCell.RowNumber > 2 && currentCell.RowNumber < 7) && (currentCell.ColumnNumber > 2 && currentCell.ColumnNumber < 7))
+                    {
+                    theGrid[currentCell.RowNumber - 2, currentCell.ColumnNumber + 1].LegalNextMove = true;
+                    theGrid[currentCell.RowNumber - 2, currentCell.ColumnNumber - 1].LegalNextMove = true;
+                    theGrid[currentCell.RowNumber + 2, currentCell.ColumnNumber + 1].LegalNextMove = true;
+                    theGrid[currentCell.RowNumber + 2, currentCell.ColumnNumber - 1].LegalNextMove = true;
+                    theGrid[currentCell.RowNumber + 1, currentCell.ColumnNumber + 2].LegalNextMove = true;
+                    theGrid[currentCell.RowNumber + 1, currentCell.ColumnNumber - 2].LegalNextMove = true;
+                    theGrid[currentCell.RowNumber - 1, currentCell.ColumnNumber + 2].LegalNextMove = true;
+                    theGrid[currentCell.RowNumber - 1, currentCell.ColumnNumber - 2].LegalNextMove = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Not valid");
+                    }
+                    break;
+
+                case "King":
+                    Console.WriteLine("The King is a slow piece that can move only one step in every direction forward, backward, to the sides or diagonall");
+                    if((currentCell.RowNumber >= 1 && currentCell.RowNumber <= 7) && (currentCell.ColumnNumber > 2 && currentCell.ColumnNumber < 7))
+                    {
+                    theGrid[currentCell.RowNumber - 1, currentCell.ColumnNumber + 0].LegalNextMove = true;
+                    theGrid[currentCell.RowNumber - 1, currentCell.ColumnNumber + 1].LegalNextMove = true;
+                    theGrid[currentCell.RowNumber - 1, currentCell.ColumnNumber - 1].LegalNextMove = true;
+                    theGrid[currentCell.RowNumber + 1, currentCell.ColumnNumber + 0].LegalNextMove = true;
+                    theGrid[currentCell.RowNumber + 1, currentCell.ColumnNumber + 1].LegalNextMove = true;
+                    theGrid[currentCell.RowNumber + 1, currentCell.ColumnNumber - 1].LegalNextMove = true;
+                    theGrid[currentCell.RowNumber + 0, currentCell.ColumnNumber + 1].LegalNextMove = true;
+                    theGrid[currentCell.RowNumber + 0, currentCell.ColumnNumber - 1].LegalNextMove = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Not valid");
+                    }
+                    break;
+
+                case "Rook":
+                    Console.WriteLine("A rook moves any number of vacant squares horizontally or verticallyl.");
+                    break;
+
+                case "Bishop":
+                    Console.WriteLine("Bishops can move only diagonal. Every bishop is confined to half of the board, as it can move only on its respective light or dark squares. . It cannot hop over other pieces like a knight");
+                    break;
+
+                case "Queen":
+                    Console.WriteLine("The Queen can move 1-7 squares in any direction, up, down, left, right, or diagonal, until the Queen reaches an obstruction or captures a piece. It cannot jump over pieces and can only capture one piece per turn.");
+                    break;
+
+                case "Pawn":
+                    Console.WriteLine("The pawn can only move forwards one step at a time, and not backwards, but when they capture the other pieces they can only do so when the opponent's piece is on a square diagonally in front of them. Each Pawn may advance two squares forward the first time it is moved");
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid choice");
+                    break;
+            }
             }
 
             for (int i = 0; i < Size; i++)
@@ -238,6 +319,7 @@ namespace Chess
 
 
                 Console.WriteLine("Do you need help?");
+                
 
                 string response = Console.ReadLine();
 
@@ -246,7 +328,6 @@ namespace Chess
 
                     myBoard.setCurrentCell();
                     myBoard.MarkNextLegalMove();
-                    Console.WriteLine("Do you need help?");
                     response = Console.ReadLine();
                 }
 
@@ -265,6 +346,7 @@ namespace Chess
                     }
                     Console.WriteLine();
                 }
+
                 for (int i = 0; i < myBoard.Size; i++)
                 {
                     for (int j = 0; j < myBoard.Size; j++)
@@ -282,10 +364,12 @@ namespace Chess
                         else
                         {
                             Console.Write("Bye");
+                            break;
                         }
                     }
-
+                   
                 }
+                
             }
         }
     }
